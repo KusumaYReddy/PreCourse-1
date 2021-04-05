caec4c8acd034098f8c53f2eedbd113a6d70146b
KusumaYReddy <64619968+KusumaYReddy@users.noreply.github.com>
2021-04-05 18:04:47 -0400

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,33 +1,50 @@
-class Stack { 
+// "static void main" must be defined in a public class.
+class Stack1 { 
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if(top==0)return true;
+        return false;
     } 
 
-    Stack() 
+    Stack1() 
     { 
+        top=0;
         //Initialize your constructor 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
+        if(top<MAX-1){
+            a[top+1]=x;
+            top++;
+            return true;
+        }
+        return false;
         //Write your code here
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+        if(isEmpty()){
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+        int temp=a[top];
+        top--;
+        return temp;
         //Write your code here
     } 
   
     int peek() 
     { 
         //Write your code here
+        return a[top];
     } 
 } 
   
@@ -35,10 +52,15 @@ class Stack {
 class Main { 
     public static void main(String args[]) 
     { 
-        Stack s = new Stack(); 
+        Stack1 s = new Stack1(); 
         s.push(10); 
         s.push(20); 
         s.push(30); 
         System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+        s.push(50);
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
+        System.out.println(s.pop() + " Popped from stack");
     } 
 }
